792cd896096a7f962db1a7d373035fb7af53ea59
r3Vibe <arnab95gupta@gmail.com>
2023-04-23 00:30:03 +0530

    query

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,25 +1,63 @@
 from django.shortcuts import render
-from .models import Product, VariationCAtegory, Variations
+from .models import VariationCAtegory, Variations, VariationCombinations
+from django.db.models import Q
 # Create your views here.
 
 
-def store_view(req, category_slug=None):
-    if req.GET.get('colour') is not None:
-        prducts = Variations.objects.filter(
-            value__iexact=req.GET.get('colour')).all()
-    elif category_slug is not None:
-        prducts = Product.objects.filter(
-            category__slug=category_slug).all().order_by('-id')
-    else:
-        prducts = Product.objects.all().order_by('-id')
+# def store_view(req, category_slug=None):
+#     if req.GET.get('colour') is not None:
+#         prducts = Variations.objects.filter(
+#             value__iexact=req.GET.get('colour')).all()
+#     elif category_slug is not None:
+#         prducts = Product.objects.filter(
+#             category__slug=category_slug).all().order_by('-id')
+#     else:
+#         prducts = Product.objects.all().order_by('-id')
 
-    count = 0
-    for item in prducts:
-        if not item.has_variation:
-            count += 1
-        else:
-            for var in item.variationcombinations_set.all():
-                count += 1
+#     count = 0
+#     for item in prducts:
+#         if not item.has_variation:
+#             count += 1
+#         else:
+#             for var in item.variationcombinations_set.all():
+#                 count += 1
+
+#     all_cat = VariationCAtegory.objects.all()
+
+#     filters = {}
+
+#     for cat in all_cat:
+#         filter_vals = []
+#         for vars in Variations.objects.filter(variation_category__slug=cat.slug).distinct('value'):
+#             filter_vals.append(vars.value)
+#         filters[cat.slug] = filter_vals
+
+#     context = {
+#         "products": prducts,
+#         "count": count,
+#         "filters": filters
+#     }
+
+#     return render(req, 'ecommerce/store.html', context=context)
+
+import operator
+from functools import reduce
+
+
+def store_view(req, category_slug=None):
+    if category_slug is not None:
+        products = VariationCombinations.objects.filter(
+            product__category__slug=category_slug).all().order_by("-id")
+    elif req.GET.get('colour'):
+        colours = req.GET.getlist('colour')
+        condition = reduce(
+            operator.and_, [Q(variations__value__icontains=s) for s in colours])
+        products = VariationCombinations.objects.filter(
+            condition).all().order_by("-id")
+        print(products)
+    else:
+        products = VariationCombinations.objects.all().filter(
+            product__is_active=True).order_by("-id")
 
     all_cat = VariationCAtegory.objects.all()
 
@@ -32,8 +70,8 @@ def store_view(req, category_slug=None):
         filters[cat.slug] = filter_vals
 
     context = {
-        "products": prducts,
-        "count": count,
+        "products": products,
+        "count": len(products),
         "filters": filters
     }
 
